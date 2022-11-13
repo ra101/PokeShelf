bdecc803658df6fcaaa242a17845751bfa38aa1d
Parth Agarwal <agarwal.parth.101@protonmail.com>
2022-11-13 13:18:11 +0530

    pyinsatller set encrption key

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ pyinstaller -y -F -w --clean -i ./Assets/PokeShelf.ico^
  --add-data form_wids.py;. --add-data frames.py;. --add-data res_wids.py;.^
  --add-data utils.py;.^
  --add-data LICENSE;. --workpath .\Build --distpath .\Dist\PokeShelf^
- -n PokeShelf __main__.py
+ -n PokeShelf __main__.py --key %SHELF_KEY%
 
 python post_build.py
 
