82174e97356053c22360d195f9b572f92ff7be49
Parth Agarwal <agarwal.parth.101@protonmail.com>
2022-11-16 14:23:16 +0530

    Build Optimization

--- build.bat
+++ build.bat
@@ -3,13 +3,18 @@
 pyinstaller -y -F -w --clean -i ./Assets/PokeShelf.ico^
  --collect-submodules tkinter.font --collect-submodules tkinter.filedialog^
  --collect-submodules ctypes.windll --collect-submodules json^
- --collect-submodules tkVideoPlayer --collect-submodules pyo^
+ --collect-submodules pyo.__init__ --collect-submodules pyo.server --collect-submodules pyo.players^
+ --collect-submodules PIL.Image --collect-submodules PIL.ImageTk^
+ --exclude-module PIL.ImageFilter --collect-submodules PIL.ImageOps^
+ --collect-submodules av --collect-submodules tkVideoPlayer^
  --collect-submodules pystray^
  --add-data __init__.py;. --add-data app.py;. --add-data utils.py;.^
  --add-data form_wids.py;. --add-data frames.py;. --add-data res_wids.py;.^
- --workpath .\Build --distpath .\Dist\PokeShelf --add-data LICENSE;.^
+ --upx-dir ./upx/ --workpath .\Build --distpath .\Dist\PokeShelf --add-data LICENSE;.^
  -n PokeShelf __main__.py --key %SHELF_KEY%
 
 python post_build.py
 
 @RD /S /Q .\Build
+@RD /S /Q .\..build
+@RD /S /Q .\__main__.build
